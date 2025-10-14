57ebc71960bc8e172196109c31d7fd0a5891207e
LeDucPr <124879680+LeDucPr@users.noreply.github.com>
2025-10-14 22:05:13 +0700

    test recursion connect

--- SystemController/Program.cs
+++ SystemController/Program.cs
@@ -1,8 +1,10 @@
-﻿using DbFactory;
+﻿using CommonDb.Connections;
+using DbFactory;
 using Enums;
 using HandlerFlow.AlgObjs.CtrlObjs;
 using HandlerFlow.AlgObjs.CtrlObjs.Connections;
 using HandlerFlow.AlgObjs.RaisingRouters;
+using PostgresExtCm.Connections;
 
 namespace SystemController;
 
@@ -10,21 +12,36 @@ class Program
 {
     static void Main(string[] args)
     {
+        string initializationConnectString =
+            "Host=localhost;Port=5432;Database=np_on_db;Username=postgres;Password=password";
         // kết nối đầu tiên được tạo bởi Pg (when deploy on server)
-        IDbFactoryWrapper factoryWrapper =
-            new DbFactoryWrapper(
-                "Host=localhost;Port=5432;Database=np_on_db;Username=postgres;Password=password",
-                EDb.Postgres
-            );
-        
+        IDbFactoryWrapper factoryWrapper = new DbFactoryWrapper(initializationConnectString, EDb.Postgres);
         ConnectionCtrl connectionCtrlDecoy = new ConnectionCtrl() // starter (chim mồi)
         {
             Id = 1,
             ConnectionInfoId = 1,
         };
 
+        HashSet<string?> connectionStrings = new HashSet<string?>();
+        connectionStrings.Add(initializationConnectString);
+
         InitialConnection initialConnection = new InitialConnection(factoryWrapper, connectionCtrlDecoy);
         BaseCtrl ctrl = initialConnection.InitializationObject;
         // var aaaaaaaaaa = initialConnection.FirstInitializationSessionId?.GetLookupData();
+
+        if (ctrl is not ConnectionCtrl connectionCtrl)
+            return;
+        ConnectionInfoCtrl? connectionInfoCtrl = connectionCtrl.ConnectionInfo;
+        int countConnection = connectionStrings.Count;
+        if (string.IsNullOrWhiteSpace(connectionInfoCtrl?.ConnectString))
+            return;
+        connectionStrings.Add(connectionInfoCtrl.ConnectString);
+        if (connectionStrings.Count == countConnection) 
+            return;
+        
+        INpOnConnectOptions postgresOptions = new PostgresConnectOptions()
+            .SetConnectionString(connectionInfoCtrl.ConnectString);
+        IDbDriverFactory dbFactory = new DbDriverFactory(EDb.Postgres, postgresOptions);
+        
     }
 }
