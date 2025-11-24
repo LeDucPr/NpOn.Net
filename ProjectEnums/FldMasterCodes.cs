64f9ab3df08e44fc803ed29f83d798ea48a3251b
chronicspec <thaiduong15321@gmail.com>
2025-11-24 16:39:13 +0700

    update file FldMasterCodes

---
+++ ProjectEnums/FldMasterCodes.cs
@@ -0,0 +1,39 @@
+namespace ProjectEnums;
+
+public static class FldMasterCodes
+{
+    /// <summary>
+    /// Query code:
+    /// </summary>
+    public const string QuestionsBySurveyId = "questions_by_survey_id";
+
+    /// <summary>
+    /// Query code:
+    /// </summary>
+    public const string SurveyCreatedAt = "survey_created_at";
+
+    /// <summary>
+    /// Query code:
+    /// </summary>
+    public const string QuestionQuestionOrder = "question_question_order";
+
+    /// <summary>
+    /// Query code:
+    /// </summary>
+    public const string UserTotalScore = "user_total_score";
+
+    /// <summary>
+    /// Query code:
+    /// </summary>
+    public const string QuestionsAnswerUser = "questions_answer_user";
+
+    /// <summary>
+    /// Query code:
+    /// </summary>
+    public const string HistoryCreatedAt = "history_created_at";
+
+    /// <summary>
+    /// Query code:
+    /// </summary>
+    public const string AnswerOrderSort = "answer_order_sort";
+}
