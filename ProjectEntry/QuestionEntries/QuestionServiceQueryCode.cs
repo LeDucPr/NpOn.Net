eebd2f3ae81771cb0ae41ce69450c3b5e0eba3f1
chronicspec <thaiduong15321@gmail.com>
2025-11-24 23:19:22 +0700

    fix conflix QuestionServiceQueryCode

--- ProjectEntry/QuestionEntries/QuestionServiceQueryCode.cs
+++ ProjectEntry/QuestionEntries/QuestionServiceQueryCode.cs
@@ -3,4 +3,8 @@ namespace ProjectEntry.QuestionEntries;
 public static class QuestionServiceQueryCode
 {
     public const string QuestionsBySurveyId = "questions_by_survey_id";
+    
+    public const string UserAnswerAdd = "user_answer_add";
+    
+    public const string UserAnswerUpdate = "user_answer_update";
 }
