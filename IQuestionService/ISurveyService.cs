2caff601b23e6123e01ae8cc3b56de7a67a09fd7
LeDucPr <124879680+LeDucPr@users.noreply.github.com>
2025-11-24 15:58:53 +0700

    c1

--- IQuestionService/ISurveyService.cs
+++ IQuestionService/ISurveyService.cs
@@ -1,5 +1,4 @@
 ﻿using CommonGrpcObject;
-using QuestionServiceObject.BusinessObjects;
 using System.ServiceModel;
 using CommonDb.DbResults.Grpc;
 using QuestionServiceObject.QueryObjects;
@@ -9,22 +8,6 @@ namespace IQuestionService;
 [ServiceContract]
 public interface ISurveyService
 {
-    /// <summary>
-    /// Lấy danh sách tất cả surveys
-    /// </summary>
-    [OperationContract]
-    Task<CommonResponse<List<QuesSrvDiseaseObject>>> GetAllSurveys();
-
-    
-    /// <summary>
-    /// Lấy danh sách questions của survey
-    /// </summary>
     [OperationContract]
     Task<CommonResponse<INpOnGrpcObject>> GetQuestionsBySurveyId(SurveyGetAllQuery query);
-    
-    /// <summary>
-    /// Lấy danh sách questions của survey
-    /// </summary>
-    [OperationContract]
-    Task<CommonResponse<List<QuestionObject>>> GetQuestionsBySurvey(SurveyGetAllQuery query);
 }
--- QuestionService/Services/SurveyService.cs
+++ QuestionService/Services/SurveyService.cs
@@ -1,63 +1,18 @@
-﻿using CommonDb.DbCommands;
-using CommonDb.DbResults;
-using CommonDb.DbResults.Grpc;
+﻿using CommonDb.DbResults.Grpc;
 using CommonGrpcObject;
 using CommonWebApplication.Services;
-using DbFactory;
 using GeneralServiceObject.QueryObjects;
-using HandleFlow.ResultConverters;
 using IGeneralService;
 using IQuestionService;
-using NpgsqlTypes;
-using QuestionServiceObject.BusinessObjects;
 using QuestionServiceObject.QueryObjects;
 
 namespace QuestionService.Services;
 
 public class SurveyService(
-    IDbFactoryWrapper dbFactoryWrapper,
     IFldMasterPgService fldMasterPgService,
     ILogger<CommonService> logger
 ) : CommonService(logger), ISurveyService
 {
-    /// <summary>
-    /// Lấy danh sách tất cả surveys
-    /// </summary>
-    public async Task<CommonResponse<List<QuesSrvDiseaseObject>>> GetAllSurveys()
-    {
-        return await CommonProcess<List<QuesSrvDiseaseObject>>(async (response) =>
-        {
-            string pgQuery = @"
-                    SELECT 
-                        id,
-                        title,
-                        description,
-                        max_total_score,
-                        is_published,
-                        created_at,
-                        updated_at
-                    FROM surveys
-                    WHERE is_published = true
-                    ORDER BY created_at DESC";
-
-            INpOnWrapperResult? resultOfQuery = await dbFactoryWrapper.QueryAsync(pgQuery);
-
-            List<QuesSrvDiseaseObject>? surveyObjects = resultOfQuery?
-                .GenericConverter(typeof(QuesSrvDiseaseObject))?
-                .Cast<QuesSrvDiseaseObject>()
-                .ToList();
-
-            if (surveyObjects is not { Count: > 0 })
-            {
-                response.SetFail("Không tìm thấy survey nào");
-                return;
-            }
-
-            response.Data = surveyObjects;
-            response.SetSuccess();
-        });
-    }
-
     public async Task<CommonResponse<INpOnGrpcObject>> GetQuestionsBySurveyId(SurveyGetAllQuery query)
     {
         return await CommonProcess<INpOnGrpcObject>(async (response) =>
@@ -83,43 +38,6 @@ public class SurveyService(
             }
 
             response.Data = questionGrpTable;
-            response.SetSuccess();
-        });
-    }
-
-    /// <summary>
-    /// Lấy danh sách questions của survey
-    /// </summary>
-    public async Task<CommonResponse<List<QuestionObject>>> GetQuestionsBySurvey(SurveyGetAllQuery query)
-    {
-        return await CommonProcess<List<QuestionObject>>(async (response) =>
-        {
-            string queryString = @"
-                    SELECT * FROM ques_srv_question
-                        WHERE ques_srv_survey_id = @ques_srv_survey_id
-                        ORDER BY question_order
-                    ";
-
-            NpOnDbCommandParam param = new NpOnDbCommandParam<NpgsqlDbType>
-            {
-                ParamName = "ques_srv_survey_id",
-                ParamValue = query.SurveyId,
-                ParamType = NpgsqlDbType.Uuid,
-            };
-            INpOnWrapperResult? wrapperResult = await dbFactoryWrapper.QueryAsync(queryString, [param]);
-
-            List<QuestionObject>? questionObjects = wrapperResult?
-                .GenericConverter(typeof(QuestionObject))?
-                .Cast<QuestionObject>()
-                .ToList();
-
-            if (questionObjects is not { Count: > 0 })
-            {
-                response.SetFail("Không tìm thấy survey");
-                return;
-            }
-
-            response.Data = questionObjects;
             response.SetSuccess();
         });
     }
