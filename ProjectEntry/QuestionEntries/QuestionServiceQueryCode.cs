2009e0ee0666329e29dd53bc96ff575f75cf6326
chronicspec <thaiduong15321@gmail.com>
2025-11-24 23:12:33 +0700

    update QuestionServiceQueryCode

--- ProjectEntry/QuestionEntries/QuestionServiceQueryCode.cs
+++ ProjectEntry/QuestionEntries/QuestionServiceQueryCode.cs
@@ -3,4 +3,17 @@ namespace ProjectEntry.QuestionEntries;
 public static class QuestionServiceQueryCode
 {
     public const string QuestionsBySurveyId = "questions_by_survey_id";
+    
+    public const string SurveyCreatedAt = "survey_created_at";
+    
+    public const string QuestionQuestionOrder = "question_question_order";
+    
+    public const string UserTotalScore = "user_total_score";
+    
+    public const string QuestionsAnswerUser = "questions_answer_user";
+    
+    public const string HistoryCreatedAt = "history_created_at";
+    
+    public const string AnswerOrderSort = "answer_order_sort";
+    
 }
